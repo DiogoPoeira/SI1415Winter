9452c80cbdd69059f41429d961c08d2cd370a962
lcduarte <lois_duarte@msn.com>
2014-11-07 16:52:56 +0000

    Adds jars;

--- src/main/java/Execute.java
+++ src/main/java/Execute.java
@@ -1,7 +1,8 @@
 import java.security.cert.Certificate;
-import java.security.cert.CertificateExpiredException;
-import java.security.cert.CertificateNotYetValidException;
+import java.security.cert.CertificateParsingException;
 import java.security.cert.X509Certificate;
+import java.util.Collection;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.net.ssl.HttpsURLConnection;
@@ -9,44 +10,63 @@ import javax.net.ssl.SSLPeerUnverifiedException;
 
 import URLConnection.URLConnectionManager;
 
-
 public class Execute {
-	
+
 	public static void main(String [] args) throws SSLPeerUnverifiedException{
 		Scanner in = new Scanner(System.in);
 		System.out.println("Please insert the desired URL!");
-		int valid = 0;
-		String urlname = in.nextLine();
+		String urlname;// = in.nextLine();
+		urlname = "https://www.google.pt";
+
 		HttpsURLConnection connection = URLConnectionManager.getConnection(urlname);
 		Certificate[] certificates = connection.getServerCertificates();
 		X509Certificate earliestExpirationDateCert = null;
+
 		for(Certificate c : certificates){
-			//System.out.println(c.toString());
-			//https://www.google.pt
+
 			if(c instanceof X509Certificate){
-				try {
-					if(earliestExpirationDateCert==null)
-						earliestExpirationDateCert=((X509Certificate)c);
-					else{
-						if(earliestExpirationDateCert.getNotAfter().after(((X509Certificate)c).getNotAfter()))
-							earliestExpirationDateCert = ((X509Certificate)c);
-					}
-					((X509Certificate)c).checkValidity();
-				} catch (CertificateExpiredException e) {
-					System.out.println("The Certificate has expired!");
-				} catch (CertificateNotYetValidException e) {
-					System.out.println("The Certificate is not yet valid!");
+				X509Certificate myCert = (X509Certificate) c;
+
+				System.out.println("\n************************************************************************ \n");
+
+				System.out.println("Issuer details - " + myCert.getIssuerDN());
+				System.out.println("Serial number - " + myCert.getSerialNumber());
+				
+				if(earliestExpirationDateCert == null){
+					earliestExpirationDateCert= myCert;
+					printHostNameValidity(myCert);
 				}
-				finally{
-					valid++;}
+				else{
+					if(earliestExpirationDateCert.getNotAfter().after(myCert.getNotAfter()))
+						earliestExpirationDateCert = myCert;
+				}
+
+				System.out.println("Data de expiração do certificado - " + myCert.getNotAfter());
+				System.out.println("Data de criação do certificado - " + myCert.getNotBefore());
+				System.out.println("Versão de TLS/SSL - " + myCert.getVersion());
 			}
 			else
 				System.out.println("Certificate not known : " + c.getType());
 		}
-		if(valid==certificates.length)
-			System.out.println("The Certificate chain is valid!");
-		connection.disconnect();
 
+		System.out.println("\n************************************************************************ \n");
+
+		connection.disconnect();
+		in.close();
 	}
 
+	private static void printHostNameValidity(X509Certificate cert) {
+
+		try {
+			Collection<List<?>> alternativeNames = cert.getSubjectAlternativeNames();
+			System.out.println("Nomes para os quais o certificado é válido : ");
+			for (List<?> s : alternativeNames){
+				System.out.println(s.get(1).toString());
+			}
+
+		} catch (CertificateParsingException e) {
+			e.printStackTrace();
+			System.out.println("Error parsing certificate.");
+		}
+	}
 }
