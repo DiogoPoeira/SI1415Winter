c6e5bdbe8138d50b01ea0f5a0b0f5bba13e6ea07
lcduarte <lois_duarte@msn.com>
2014-12-28 16:28:57 +0000

    Adds support to RBAC1;

--- T3/Ex8/src/main/java/PDP/Core.java
+++ T3/Ex8/src/main/java/PDP/Core.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import JSONDeserializer.RoleDeserializer;
 import JSONDeserializer.UserDeserializer;
@@ -32,20 +33,32 @@ public class Core {
 	}
 	
 	public boolean getResource(User user, Permissions permission, String resource){
-		for (Role role : user.getRoles()) {
-			List<Permissions> permissionsList = role.getPermissionsList();
-			if (permissionsList.contains(permission)) {
-				for(Permissions perm : permissionsList){
-					if (perm.equals(permission) && perm.getResourceList().contains(resource)){
-						return true;
-					}
-				}
+		return getResource(user.getRoles(), permission, resource);
+	}
+	
+	private boolean getResource(List<Role> roles, Permissions permission, String resource) {
+		for (Role role : roles) {
+			if (checkForPermissionInSubRoles(role, permission, resource)){
+				return true;
 			}
 		}
 		
 		return false;
 	}
-	
+
+	private boolean checkForPermissionInSubRoles(Role role, Permissions permission, String resource) {
+		List<Permissions> permissionsList = role.getPermissionsList();
+		if (permissionsList.contains(permission)) {
+			for(Permissions perm : permissionsList){
+				if (perm.equals(permission) && perm.getResourceList().contains(resource)){
+					return true;
+				}
+			}
+		}
+		
+		return getResource(role.getSubRoles(), permission, resource);
+	}
+
 	private void load(){
 		try (InputStream stream = Core.class.getClassLoader().getResourceAsStream("res/permissionsFile.json");
 				BufferedReader br = new BufferedReader(new InputStreamReader(stream))){
@@ -61,9 +74,11 @@ public class Core {
 			
 			JsonElement obj = parser.parse(buffer.toString());
 			JsonArray usersJson = obj.getAsJsonObject().get("userArray").getAsJsonArray(),
-					  roleJson = obj.getAsJsonObject().get("roleArray").getAsJsonArray();
+					  roleJson = obj.getAsJsonObject().get("roleArray").getAsJsonArray(),
+					  roleHierarchy = obj.getAsJsonObject().get("roleHierarchy").getAsJsonArray();
 			
 			loadRolesFromJson(roleJson);
+			createRoleHierarchy(roleHierarchy);
 			loadUsersFromJson(usersJson);
 		
 		} 
@@ -71,6 +86,28 @@ public class Core {
 			System.out.println("Failed reading the file with the permissions and stuff");
 		}
 		System.out.println();
+	}
+
+	private void createRoleHierarchy(JsonArray roleHierarchy) {
+		for(JsonElement roleDependency : roleHierarchy){
+			String roleId = roleDependency.getAsJsonObject().get("Role").getAsString();
+			JsonArray subRoles = roleDependency.getAsJsonObject().get("ContainedRoles").getAsJsonArray();
+			
+			Role masterRole = findRole(roleId);
+			
+			for (JsonElement subRole : subRoles){
+				masterRole.addSubRole(findRole(subRole.getAsString()));
+			}
+		}
+	}
+
+	private Role findRole(String roleId) {
+		for (Role role : roleList){
+			if (role.id.equals(roleId))
+				return role;
+		}
+		
+		throw new NoSuchElementException();
 	}
 
 	private void loadRolesFromJson(JsonArray roleJson) {
--- T3/Ex8/src/main/java/Roles/Role.java
+++ T3/Ex8/src/main/java/Roles/Role.java
@@ -9,10 +9,12 @@ public class Role {
 	
 	public final String id;
 	private List<Permissions> permissionList;
+	private List<Role> subRoles;
 
 	public Role( String id ){
 		this.id = id;
 		this.permissionList = new ArrayList<Permissions>();
+		this.subRoles = new ArrayList<Role>();
 	}
 	
 	public void addPermission(Permissions permission){
@@ -23,6 +25,16 @@ public class Role {
 	public List<Permissions> getPermissionsList(){
 		return permissionList;
 	}
+	
+	public void addSubRole(Role role){
+		if(!subRoles.contains(role))
+			subRoles.add(role);
+	}
+	
+	public List<Role> getSubRoles(){
+		return subRoles;
+	}
+	
 	
 	@Override
 	public boolean equals(Object obj) {
